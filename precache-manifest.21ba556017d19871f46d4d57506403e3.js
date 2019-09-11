2b8aa0c5af2003b877ab06bd06e1d7efd8d04720
Michal Janowski <michaljanjanowski@gmail.com>
2019-09-11 17:10:46 +0200

    Updates

---
+++ precache-manifest.21ba556017d19871f46d4d57506403e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55371086e36e27d7d896fe1f9cd5df7e",
+    "url": "/tic_tac_toe/index.html"
+  },
+  {
+    "revision": "d2f5fded7154fe80de6f",
+    "url": "/tic_tac_toe/static/css/main.d0703136.chunk.css"
+  },
+  {
+    "revision": "6f3cc996c0af99449f21",
+    "url": "/tic_tac_toe/static/js/2.2a5690ad.chunk.js"
+  },
+  {
+    "revision": "d2f5fded7154fe80de6f",
+    "url": "/tic_tac_toe/static/js/main.7a32edf7.chunk.js"
+  },
+  {
+    "revision": "2380040020d3c2f3c575",
+    "url": "/tic_tac_toe/static/js/runtime~main.84bd0879.js"
+  }
+]);
